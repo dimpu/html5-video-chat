f274a926eaed9af9a0b7552d2ee05230ea9eec98
dimpu <buddhaaravind@gmail.com>
2017-09-11 22:34:41 -0400

    WIP

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,33 +1,26 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-// Import routing components
-import {Router, Route} from 'react-router';
-import RaisedButton from 'material-ui/RaisedButton';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import rootReducer from './store/reducers';
+import { fetchMessages, checkUserExists } from './store/actions';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    )
+);
 
 
-export class Home extends Component {
-    render(){
-        return (<RaisedButton label="Default" />);
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+               <LoginOrChat />
+            </Provider>
+        );
     }
 }
-
-// More components
-export class Car extends Component {
-    render(){
-        return (<h1>Cars page</h1>);
-    }
-}
-
-export class About extends Component {
-    render(){
-        return (<h1>About page</h1>);
-    }
-}
-
-
-
-
-
-// render(element,
-//   document.getElementById('root')
-// );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-// import ReactRouter,{Router, Route } from 'react-router';
 import { BrowserRouter, Route } from 'react-router-dom'
-import  Home  from './app/components/home.jsx';
-import  Login  from './app/components/login.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-
-// document.head.innerHTML =  document.head.innerHTML + `<base href="/" />`;
-// document.body.innerHTML = document.body.innerHTML + `
-//   <div id="root"></div>
-// `;
-
+// app code 
+import  Home  from './app/components/home.jsx';
+import  Login  from './app/components/login.jsx';
 
 
 render(
@@ -27,7 +21,3 @@ render(
     document.getElementById('root')
 );
 
-
-// render(element,
-//   document.getElementById('root')
-// );
